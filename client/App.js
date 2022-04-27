c4b6edef698dafbc2daaa5cc64e752e35c3e5744
Joshua Krauchi <jkrauchi@my.bcit.ca>
2022-04-26 18:56:05 -0700

    Added basic OpenLayers map.

--- client/App.js
+++ client/App.js
@@ -5,9 +5,7 @@ export default function App() {
   return (
     <SafeAreaView style={tailwind("flex-1 items-center justify-center")}>
       <View style={tailwind("bg-blue-500 px-5 py-3 rounded-full")}>
-        <Text style={tailwind("text-white font-semibold text-lg")}>
-          Hello Tailwind 👋 POG
-        </Text>
+        <Text style={tailwind("text-white font-semibold text-lg")}>Hello Tailwind 👋 POG</Text>
       </View>
     </SafeAreaView>
   );

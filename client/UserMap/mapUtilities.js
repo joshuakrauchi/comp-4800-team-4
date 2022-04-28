c04427e769b7c4f3680537e6c3bddea946b376d6
Joshua Krauchi <jkrauchi@my.bcit.ca>
2022-04-27 23:16:11 -0700

    Added general styling for the map.

---
+++ client/UserMap/mapUtilities.js
@@ -0,0 +1,107 @@
+import Map from "ol/Map";
+import TileLayer from "ol/layer/Tile";
+import MapView from "ol/View";
+import Feature from "ol/Feature";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
+import { Attribution, defaults as defaultControls } from "ol/control";
+import { Icon, Style } from "ol/style";
+import { Point } from "ol/geom";
+
+import pinData from "../data/pinData";
+import pinImage from "../images/pinImage.png";
+
+// Create a pin vector image layer for adding to the map.
+const createPinLayer = (lon, lat, name, image) => {
+  let coords = fromLonLat([lon, lat]);
+  const pinFeature = new Feature({
+    geometry: new Point(coords),
+  });
+
+  const pinSource = new VectorSource({
+    features: [pinFeature],
+  });
+
+  const pinStyle = new Style({
+    image: new Icon({
+      src: image,
+      scale: 1.0,
+      opacity: 0.85,
+    }),
+  });
+
+  return new VectorLayer({
+    source: pinSource,
+    style: pinStyle,
+    name: name,
+  });
+};
+
+// Try to get the user's position. If allowed, update their position as much as possible.
+const tryWatchLocation = (userPin) => {
+  if (!navigator.geolocation) return;
+
+  // Update the location if given permission.
+  navigator.geolocation.watchPosition((position) => {
+    updateLocation(position, userPin);
+  });
+};
+
+const updateLocation = (currentPosition, userPin) => {
+  let coords = currentPosition.coords;
+  userPin
+    .getSource()
+    .getFeatures()[0]
+    .getGeometry()
+    .setCoordinates(fromLonLat([coords.longitude, coords.latitude]));
+};
+
+const addBadgePins = (map) => {
+  Object.keys(pinData).forEach((key) => {
+    let name = key;
+    let crab = pinData[key];
+
+    map.addLayer(createPinLayer(crab.lon, crab.lat, name, crab.pinImage));
+    map.addLayer(createPinLayer(crab.lon, crab.lat - 0.0001, name, crab.crabImage));
+  });
+};
+
+const addUserPin = () => {
+  return createPinLayer(0, 0, "Your Location", pinImage);
+}
+
+const createMap = (mapElement) => {
+  // Change the default map controls to get rid of the regular attributions
+  // on the bottom right and replace them with a collapsible button.
+  const attribution = new Attribution({
+    collapsible: true,
+  });
+
+  const mapControls = defaultControls({ attribution: false, zoom: false }).extend([attribution]);
+
+  const initialMap = new Map({
+    target: mapElement.current,
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new MapView({
+      center: fromLonLat([37.41, 8.82]),
+      zoom: 4,
+    }),
+    controls: mapControls,
+  });
+
+  initialMap.on("singleclick", (event) => {
+    initialMap.forEachFeatureAtPixel(event.pixel, (feature) => {
+      console.log(feature.getGeometry().getFlatCoordinates());
+    });
+  });
+
+  return initialMap;
+};
+
+export { tryWatchLocation, addBadgePins, addUserPin, createMap };

f11b99e18ff9d1939cb44e441c0b8f0e4fa08399
MoBaydoun <mbaydoun@my.bcit.ca>
2022-04-28 14:59:20 -0700

    Reconfigured app to regular React with TypeScript and Tailwind.

---
+++ client/src/components/IconButton.tsx
@@ -0,0 +1,7 @@
+
+
+const IconButton = () => {
+
+};
+
+export default IconButton;
--- client/utilities/constants.js
+++
@@ -1,8 +0,0 @@
-const InfoBoxItem_1 = [
-  'Monitoring the Dungeness crab gives scientists insight into how the rest of the ecosystem is doing.',
-  'We are using a network of humane traps along the Salish Sea to attract Dungeness megalopae using light. Find out what we have learned about these crabs!'
-];
-
-export {
-  InfoBoxItem_1
-};

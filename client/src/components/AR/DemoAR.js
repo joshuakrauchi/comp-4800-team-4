f370d10a2ea63ddf9404c25c661cbd07b3f26905
Sean Clayton <sean.clayto@gmail.com>
2022-05-02 18:43:22 -0700

    still trying to get it working

--- client/src/components/AR/DemoAR.js
+++ client/src/components/AR/DemoAR.js
@@ -2,18 +2,18 @@
 
 const DemoAR = () => {
     return (
-        <a-scene embedded arjs='sourceType: webcam;'>
-            <a-marker-camera preset='hiro'>
-                <a-box color='orange' scale='1 1 1' position='0 0.5 0' material='opacity: 1;'>
-                    <a-animation 
-                        attribute='rotation' 
-                        to='360 360 0' 
-                        dur='3000' 
-                        easing='linear' 
-                        repeat='indefinite'>
-                    </a-animation>
-                </a-box>
-            </a-marker-camera>
+        // <a-scene embedded arjs='sourceType: webcam;'>
+        <a-scene embedded arjs>
+            <a-box color='orange' scale='0.75 0.75 0.75' position='0 0.37 0' material='opacity: 1;'>
+                {/* <a-animation 
+                    attribute='rotation' 
+                    to='360 360 0' 
+                    dur='3000' 
+                    easing='linear' 
+                    repeat='indefinite'>
+                </a-animation> */}
+            </a-box>
+            <a-marker-camera preset='hiro'></a-marker-camera>
         </a-scene>
     );
 };

bb59eb4f1f7ae8345ad038c97a592c0445dad60b
Sean Clayton <sean.clayto@gmail.com>
2022-05-02 17:34:54 -0700

    trying to fix camera location

--- client/src/components/AR/DemoAR.js
+++ client/src/components/AR/DemoAR.js
@@ -3,16 +3,17 @@
 const DemoAR = () => {
     return (
         <a-scene embedded arjs='sourceType: webcam;'>
-            <a-box color='orange' position='0 0.05 0' material='opacity: 1;'>
-                <a-animation 
-                    attribute='rotation' 
-                    to='360 360 0' 
-                    dur='3000' 
-                    easing='linear' 
-                    repeat='indefinite'>
-                </a-animation>
-            </a-box>
-            <a-marker-camera preset='hiro'></a-marker-camera>
+            <a-marker-camera preset='hiro'>
+                <a-box color='orange' position='0 0.05 0' material='opacity: 1;'>
+                    <a-animation 
+                        attribute='rotation' 
+                        to='360 360 0' 
+                        dur='3000' 
+                        easing='linear' 
+                        repeat='indefinite'>
+                    </a-animation>
+                </a-box>
+            </a-marker-camera>
         </a-scene>
     );
 };

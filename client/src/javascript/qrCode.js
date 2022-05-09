6ca03a846f097ec60a5a97d0de8afff6151b14d3
PariyaKasaeian <pkasaiyan@gmail.com>
2022-05-08 17:40:53 -0700

    Changed the function a little bit.

--- client/src/javascript/qrCode.js
+++ client/src/javascript/qrCode.js
@@ -4,16 +4,6 @@ import { createSearchParams, useSearchParams } from "react-router-dom";
 const qrCode = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    /*
-    !! set in the main page !!
-
-    -> false = not scanned
-    localStorage.setItem("badgeCrab1", "false");
-    localStorage.setItem("badgeCrab2", "false");
-    ...
-
-    */
-
     const successMessages = ["just a few more crabs to know!", "You found it!", "Amazing!", "Another one!"];
     const errorMessages = ["Oops!", "Uh oh!", "Oh no!"];
 
@@ -22,22 +12,24 @@ const qrCode = () => {
         return messages[index];
     };
 
-    if (localStorage.getItem(searchParams) == "false") {
-        localStorage.setItem(searchParams, "true");
-        return (
+    const getRandomMessage = (messages) => {
+        const index = Math.floor(Math.random() * messages.length);
+        return messages[index];
+    };
+
+    if (localStorage.getItem(searchParams) !== null) {
+        return (<p>
+            <span>
+                {getRandomMessage(errorMessages)}
+            </span> Seems like you've caught this crab before!
+        </p>);
+    } else {
+        return(
             <p>
                 <span>
                     {getRandomMessage(successMessages)} 
                 </span> Let's go find the next one!
             </p>
         );
-    }
-
-    return (
-        <p>
-            <span>
-                {getRandomMessage(errorMessages)}
-            </span> Seems like you've caught this crab before!
-        </p>
-    );
+    };
 };

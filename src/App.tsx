097b09155a85397e217df3849eff9cb395a02700
MoBaydoun <mbaydoun@my.bcit.ca>
2022-05-10 11:39:51 -0700

    Added a router to display all the features.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import MapPage from "./pages/MapPage";
 import DemoARPage from "./pages/DemoARPage";
+import BadgeModal from "./components/BadgeModal/BadgeModal";
+
+import boulder from "./images/boulder.png";
 
 const App = (): JSX.Element => {
-  return <MapPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path={"/*"} element={<MapPage />} />
+        <Route path={"/ar"} element={<DemoARPage />} />
+        <Route path={"/badge"} element={<BadgeModal badge={boulder} badgeName="Young Crab"/>} />
+        <Route path={"/qr"} element={<div>does not exist</div>} />
+        <Route path={"/trivia"} element={<div>does not exist</div>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

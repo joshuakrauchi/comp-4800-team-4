556bf0a93634698c3bc689009ae2ba019d33d729
Joshua Krauchi <jkrauchi@my.bcit.ca>
2022-05-17 14:37:43 -0700

    Added a function to check what badges were found.

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,12 +1,14 @@
+import { useEffect, useRef } from "react";
+
 import BadgeMap from "../components/BadgeMap/BadgeMap";
 import InfoBox from "../components/InfoBox";
 import Button from "../components/Button";
-
-import { useEffect, useRef } from "react";
+import { useBadge } from "../context/BadgeContext";
 import BadgeWidget from "../components/BadgeWidget/BadgeWidget";
 
 const MapPage = (): JSX.Element => {
-  const foundBadges = useRef([true, true, false, false]);
+  const { GetFoundBadges } = useBadge();
+  const foundBadges = useRef(GetFoundBadges());
   const styles = {
     mainContainer:
       "App px-10 py-8 overflow-scroll bg-background flex-col justify-center align-center h-screen w-screen",
@@ -20,7 +22,7 @@ const MapPage = (): JSX.Element => {
     <div className={styles.mainContainer}>
       <h1 className={styles.headerText}>Collect All Of The Badges!</h1>
       <BadgeWidget foundBadges={foundBadges.current} />
-      <BadgeMap />
+      <BadgeMap foundBadges={foundBadges.current} />
       <InfoBox
         information={[
           "Monitoring the Dungeness crab gives scientists insight into how the rest of the ecosystem is doing.",

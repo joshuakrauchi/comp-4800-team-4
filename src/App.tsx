2307933cecc0bf617535150040bf8ab05233f998
Mazianos <mazin.marwan@gmail.com>
2022-05-17 13:11:32 -0700

    removing test lines

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,14 @@ import MapPage from "./pages/MapPage";
 import BadgeModal from "./components/BadgeModal/BadgeModal";
 
 import boulder from "./images/boulder.png";
-import { useBadge } from "./context/BadgeContext";
 
 const App = (): JSX.Element => {
-  const { AddBadge } = useBadge();
-  AddBadge("One");
   return (
     <Router>
       <Routes>
         <Route path={"/*"} element={<MapPage />} />
         <Route path={"/badge"} element={<BadgeModal badge={boulder} badgeName="Young Crab" callback={() => {window.location.href="/"}}/>} />
         <Route path={"/qr"} element={<div>does not exist</div>} />
-        <Route path={"/trivia"} element={<div>does not exist</div>} />
-        <Route path={"/trivia2"} element={<div>does not exist</div>} />
-        <Route path={"/trivia3"} element={<div>does not exist</div>} />
-        <Route path={"/trivia4"} element={<div>does not exist</div>} />
       </Routes>
     </Router>
   );

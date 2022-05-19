61f5365d03a064a7bbb7c85059c9250797db5dc7
PariyaKasaeian <pkasaiyan@gmail.com>
2022-05-19 12:51:53 -0700

    Added styling to the page.

--- src/pages/AllStampsEarned.tsx
+++ src/pages/AllStampsEarned.tsx
@@ -2,13 +2,17 @@ import Button, { ButtonProp } from "../components/Button";
 import CongratsStamp from "../components/CongratsStamp";
 
 const AllStampsEarned = (): JSX.Element => {
+
+    const style = {
+        container: "App overflow-scroll bg-background h-screen",
+    };
+
     return (
-        <div>
+        <div className={style.container}>
             <CongratsStamp/>
             <Button text="Download the App"/>
             <Button text="Learn More"/>
-        </div>
-        
+        </div>     
     );
 };
 

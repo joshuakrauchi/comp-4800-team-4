d9e1e5eaa40f86186c62fa2ce1d33de140328502
PariyaKasaeian <pkasaiyan@gmail.com>
2022-05-19 13:14:09 -0700

    Formatted the documents.

--- src/components/CongratsStamp.tsx
+++ src/components/CongratsStamp.tsx
@@ -1,21 +1,33 @@
 import Naturalist from "../images/naturalist.png";
 
 const CongratsStamp = (): JSX.Element => {
+  const style = {
+    container: "bg-popout m-8 rounded-lg",
+    head: "text-2xl font-bold text-white text-center p-7 pt-16 pb-2 align-center",
+    par: " p-8 pt-5 pb-5 font-semibold leading-relaxed text-white text-center align-center",
+    img: "w-full pt-7 pb-14",
+  };
 
-    const style = {
-        container: "bg-popout m-8 rounded-lg",
-        head: "text-2xl font-bold text-white text-center p-7 pt-16 pb-2 align-center",
-        par: " p-8 pt-5 pb-5 font-semibold leading-relaxed text-white text-center align-center",
-        img: "w-full pt-7 pb-14",
-    };
-
-    return (
-        <div className={style.container} id="congrats-div">
-            <h3 className= {style.head}> Congratulations, you've collected all of the stamps! </h3>
-            <p className={style.par}> If you see any real Dungeness crabs in your journey, please snap a photo and submit it to iNaturalist to help us track and monitor these creatures. </p>
-            <img className={style.img} alt="naturalist logo" id="logo-img" src={Naturalist}/>
-        </div>
-    );
+  return (
+    <div className={style.container} id="congrats-div">
+      <h3 className={style.head}>
+        {" "}
+        Congratulations, you've collected all of the stamps!{" "}
+      </h3>
+      <p className={style.par}>
+        {" "}
+        If you see any real Dungeness crabs in your journey, please snap a photo
+        and submit it to iNaturalist to help us track and monitor these
+        creatures.{" "}
+      </p>
+      <img
+        className={style.img}
+        alt="naturalist logo"
+        id="logo-img"
+        src={Naturalist}
+      />
+    </div>
+  );
 };
 
 export default CongratsStamp;
--- src/pages/AllStampsEarned.tsx
+++ src/pages/AllStampsEarned.tsx
@@ -1,19 +1,30 @@
-import Button, { ButtonProp } from "../components/Button";
+import Button from "../components/Button";
 import CongratsStamp from "../components/CongratsStamp";
 
 const AllStampsEarned = (): JSX.Element => {
+  const style = {
+    container: "App overflow-scroll bg-background h-screen",
+  };
 
-    const style = {
-        container: "App overflow-scroll bg-background h-screen",
-    };
-
-    return (
-        <div className={style.container}>
-            <CongratsStamp/>
-            <Button text="Download the App" onClick={()=> {window.open("http://play.google.com/store/apps/details?id=org.inaturalist.android")}}/>
-            <Button text="Learn More" onClick={()=> {window.open("https://sentinels.hakai.org/about")}}/>
-        </div>     
-    );
+  return (
+    <div className={style.container}>
+      <CongratsStamp />
+      <Button
+        text="Download the App"
+        onClick={() => {
+          window.open(
+            "http://play.google.com/store/apps/details?id=org.inaturalist.android"
+          );
+        }}
+      />
+      <Button
+        text="Learn More"
+        onClick={() => {
+          window.open("https://sentinels.hakai.org/about");
+        }}
+      />
+    </div>
+  );
 };
 
 export default AllStampsEarned;

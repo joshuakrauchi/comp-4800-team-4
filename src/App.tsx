d935ec608c8e83f40ecfd13fa1f754bc13c6ae64
PariyaKasaeian <pkasaiyan@gmail.com>
2022-05-19 12:52:14 -0700

    Added the routed page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MapPage from "./pages/MapPage";
 import DemoARPage from "./pages/DemoARPage";
 import BadgeModal from "./components/BadgeModal/BadgeModal";
-
+import AllStampsEarned from "./pages/AllStampsEarned";
 import boulder from "./images/boulder.png";
 
 const App = (): JSX.Element => {
@@ -15,6 +15,7 @@ const App = (): JSX.Element => {
         <Route path={"/badge"} element={<BadgeModal badge={boulder} badgeName="Young Crab"/>} />
         <Route path={"/qr"} element={<div>does not exist</div>} />
         <Route path={"/trivia"} element={<div>does not exist</div>} />
+        <Route path={"/DownloadTheApp"} element={<AllStampsEarned/>} />
       </Routes>
     </Router>
   );

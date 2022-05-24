f06bd2a668d5ae07038ecc93bed74bdb6adc1e54
Sean Clayton <sean.clayto@gmail.com>
2022-05-23 19:06:24 -0700

    changed onboarding page selection dots

--- src/pages/OnboardPage/components/MapCrab.tsx
+++ src/pages/OnboardPage/components/MapCrab.tsx
@@ -3,7 +3,7 @@ import OnboardFooter from "./OnboardFooter";
 import sentinels from "../../../images/sentinelsLabel.png";
 import mapSnap from "../../../images/mapSnap.png";
 import crab from "../../../images/mapCrab.png";
-import selection2 from "../../../images/selection2Alt.png";
+import selection3 from "../../../images/selection3Alt.png";
 
 import styles from "./styles";
 
@@ -17,7 +17,7 @@ const MapCrab = (): JSX.Element => {
       />
       <img alt="map snapshot" className={styles.infoSnap} src={mapSnap} />
       <img alt="info crab" className={styles.infoCrab} src={crab} />
-      <OnboardFooter path={selection2} />
+      <OnboardFooter path={selection3} />
     </div>
   );
 };
